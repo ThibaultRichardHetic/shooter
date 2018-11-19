0e1d91f444db0ea01e53928f1a37898f07a2a15c
ThibaultRichardHetic <thibault.richard2000@gmail.com>
2018-11-19 17:50:54 +0100

    :sparkles: add local storage hight score

--- scripts/app.js
+++ scripts/app.js
@@ -8,11 +8,22 @@ shooter.$start = shooter.$container.querySelector('.start')
 shooter.$score = shooter.$container.querySelector('.score .value')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$targets = shooter.$container.querySelector('.targets')
+shooter.$bestScore = shooter.$container.querySelector('.best-score .value')
 shooter.score = 0
+shooter.secondsLeft = 0
+shooter.bestScore = window.localStorage.getItem('bestScore')
+
+if(shooter.bestScore === null)
+{
+    shooter.bestScore = 0
+}
+
+shooter.bestScore = parseInt(shooter.bestScore)
+shooter.$bestScore.textContent = shooter.bestScore
 
 shooter.sounds = {}
-shooter.sounds.ding = new Audio ('shooter-sounds/ding.mp3')
-shooter.sounds.finish = new Audio ('shooter-sounds/finish.mp3')
+shooter.sounds.ding = new Audio('ding.mp3')
+shooter.sounds.finish = new Audio('finish.mp3')
 
 shooter.$start.addEventListener('click', () =>
 {
@@ -22,26 +33,42 @@ shooter.$start.addEventListener('click', () =>
 /**
  * Methods
  */
-
 shooter.start = () =>
 {
-    console.log('start')
     shooter.$container.classList.remove('step-start')
+    shooter.$container.classList.remove('step-end')
     shooter.$container.classList.add('step-game')
+    
+    shooter.secondsLeft = 4
 
-    shooter.secondsLeft = 12
+    shooter.score = 0
+    shooter.$score.textContent = shooter.score
 
     shooter.tick()
+}
+
+shooter.end = () =>
+{
+    shooter.$container.classList.remove('step-game')
+    shooter.$container.classList.add('step-end')
+
+    shooter.sounds.finish.play()
+
+    if(shooter.score > shooter.bestScore)
+    {
+        window.localStorage.setItem('bestScore', shooter.score)
+        shooter.bestScore = shooter.score
+        shooter.$bestScore.textContent = shooter.score
+    }
 }
 
 shooter.tick = () =>
 {
     shooter.secondsLeft--
-    console.log(shooter.secondsLeft)
 
     if(shooter.secondsLeft === 0)
     {
-        console.log('end')
+        shooter.end()
     }
     else
     {
@@ -49,15 +76,18 @@ shooter.tick = () =>
         {
             shooter.$timer.textContent = `00:0${shooter.secondsLeft}`
         }
-        else{
+        else
+        {
             shooter.$timer.textContent = `00:${shooter.secondsLeft}`
         }
+
         window.setTimeout(shooter.tick, 1000)
     }
 }
 
 shooter.addTarget = () =>
 {
+    // Create target
     const $target = document.createElement('div')
     $target.classList.add('target')
     $target.style.top = `${Math.random() * 100}%`
@@ -67,7 +97,7 @@ shooter.addTarget = () =>
     // Listen to mouse enter
     $target.addEventListener('mouseenter', () =>
     {
-        shooter.shootTarget($target)   
+        shooter.shootTarget($target)
     })
 }
 
@@ -75,8 +105,10 @@ shooter.shootTarget = (_$target) =>
 {
     // Delete target
     _$target.remove()
+
     // Add new target
     shooter.addTarget()
+
     // Increment score
     shooter.score++
     shooter.$score.textContent = shooter.score
